8cf72361190322e161cebb65125187a577b15a26
rastalas <ianmcgeoch@gmail.com>
2014-01-07 19:15:12 -0500

    oops

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 config = {
     'username' : 'PGN_Link_Bot',
-    'password' : 'maroczybind',
+    'password' : '',
     'user_agent' : 'A bot to grab pgns to post as text in /r/chess',
     'subreddit_list' : ['chessbottesting'],
 }
