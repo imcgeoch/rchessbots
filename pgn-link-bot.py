0ec348073c82fc9e67fdd3a65bab12e4afd782cb
rastalas <ianmcgeoch@gmail.com>
2014-01-08 02:13:22 -0500

    fixed I think

--- pgn-link-bot.py
+++ pgn-link-bot.py
@@ -1,4 +1,3 @@
-import praw
 
 """ 
 A reddit bot that will reply to links to chess games on a number 
@@ -9,15 +8,17 @@ Completed: define variables
 TODO: Everything
 """
 
-# define local variables from config file
 from config import config
+import praw
+import urllib2
 
+# define local variables
 USERNAME = config['username']
 PASSWORD = config['password']
 USER_AGENT = config['user_agent']
 SUBREDDIT_LIST = config['subreddit_list']
 
-SITE_LIST = ['www.chess.com/echess/game/', 'www.chessgames.com/perl/chessgame',]
+SITE_LIST = ['www.chess.com/livechess/game', 'www.chess.com/echess/game', 'www.chessgames.com/perl/chessgame', 'en.lichess.org/']
 
 reddit = praw.Reddit(user_agent = USER_AGENT)
 reddit.login(username = USERNAME, password = PASSWORD)
@@ -28,37 +29,80 @@ print "logging in to reddit"
 
 def go():
     subreddit = reddit.get_subreddit('chessbottesting')  
-    print 'got subreddit'
-    for submission in subreddit.get_new(limit=10):
-        op_link = submission.url.lower()
-        print "pulled link %s" % op_link
-        if SITE_LIST[1] in op_link:
-            print "found link \n ========== \n\n\n\n\n\n"
-            processLinkPost(op_link)
+    for  submission in subreddit.get_new(limit=10):
+        
+        print "Checking " + submission.id
+        if not submission.is_self:
+            print "id'd as link post"
+            processLinkPost(submission)
+        else:
+            print "id'd as self post"
+            processSelfPost(submission)
 
+def processLinkPost(submission):
+    op_link = submission.url.lower()
+    for site in SITE_LIST:
+        if site in op_link and not submission.id in already_done:
+            print "found link \n ========== "
+            print submission.id + "\n\n"
+            already_done.append(submission.id)
+    
+    for p in already_done: print "Completed: \n " + p
 
-"""          
-def getPosts():
-
-def getComments():
-"""
-def processLinkPost(op_link):
-    print op_link
-"""
-def processSelfPost():
+def processSelfPost(submission):
+    op_text = submission.selftext.lower()
+    link_list = []
+    link_list = linksFromText(op_text)
 
 def processComment():
+    return 0
 
-def linksFromText():
+def linksFromText(post):
+    links_found = []
+    link_text = []
+
+    """# Catalog all locations of links to games
+    for site in SITE_LIST:
+        if site in post:
+            links_found.append(post.index(site))
+    links_found.sort()
+"""
+
+    # See where each links and use it generate the url of the DL link
+    #for i in links_found:
+    for i in range(0, len(post)-15):
+        token = post[i:i+15]
+        if token == "www.chess.com/l":
+            link_text.append("http://www.chess.com/livechess/download_pgn?id=" + post[i+32:i+40])
+            print "found chess.com/e"
+        elif token == "www.chess.com/e":
+            link_text.append("http://www.chess.com/echess/download_pgn?id=" + post[i+29:i+37])
+            print "found www.chess.com/e"
+        elif token == "www.chessgames.":
+            link_text.append("http://www.chessgames.com/perl/nph-chesspgn?text=1&gid=" + post[i+38:i+45])   
+            print "found chessgames"
+        elif token == "en.lichess.org/":
+            link_text.append("http://en.lichess.org/" + post[i+15:i+23] + "/pgn")
+            print "found lichess"
+    for a in link_text:
+        print a
+ 
+    return link_text
+        
 
 def checkLink():
+    return 0
 
-def getPgn():
+def getPgn(target):
+    response = urllib2.urlopen(target)
+    html = response.read()
+    return html
 
-def postPgn():
-"""
-while ('true'):
-    print 'preparing to go'
-    go()
+def postPgn(links):
+    post = "[pgn] " + pgn + "[/pgn]"
+       
+ 
+print 'preparing to go'
+go()
 
 
