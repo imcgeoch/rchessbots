621941925612461e5ea290d01ee7cb1c0532aace
rastalas <ianmcgeoch@gmail.com>
2014-01-08 20:58:36 -0500

    Can make pgn urls from text posts and other goodies

--- pgn-link-bot.py
+++ pgn-link-bot.py
@@ -4,8 +4,6 @@ A reddit bot that will reply to links to chess games on a number
 of off-reddit sites, take the pgns from those sites, and respond
 with a comment containing the pgn in [pgn] tags for the veiwer plugin.
 
-Completed: define variables 
-TODO: Everything
 """
 
 from config import config
@@ -16,9 +14,13 @@ import urllib2
 USERNAME = config['username']
 PASSWORD = config['password']
 USER_AGENT = config['user_agent']
-SUBREDDIT_LIST = config['subreddit_list']
+#SITE_LIST = config['subreddit_list']
 
 SITE_LIST = ['www.chess.com/livechess/game', 'www.chess.com/echess/game', 'www.chessgames.com/perl/chessgame', 'en.lichess.org/']
+
+NESTED_LIST = config['nested_list']
+
+# nest = [('www.chess.com/livechess/game', 'http://www.chess.com/livechess/download_pgn?id=', '9' , ''),('www.chess.com/echess/game', 'http://www.chess.com/echess/download_pgn?id=', '9', ''),('www.chessgames.com/perl/chessgame', 'http://www.chessgames.com/perl/nph-chesspgn?text=1&gid=', '8', ''),('en.lichess.org/', 'http://en.lichess.org/', '9', '/pgn' )]
 
 reddit = praw.Reddit(user_agent = USER_AGENT)
 reddit.login(username = USERNAME, password = PASSWORD)
@@ -31,9 +33,9 @@ def go():
     subreddit = reddit.get_subreddit('chessbottesting')  
     for  submission in subreddit.get_new(limit=10):
         
-        print "Checking " + submission.id
+  #      print "Checking " + submission.id
         if not submission.is_self:
-            print "id'd as link post"
+ #           print "id'd as link post"
             processLinkPost(submission)
         else:
             print "id'd as self post"
@@ -41,13 +43,13 @@ def go():
 
 def processLinkPost(submission):
     op_link = submission.url.lower()
-    for site in SITE_LIST:
-        if site in op_link and not submission.id in already_done:
-            print "found link \n ========== "
-            print submission.id + "\n\n"
+    for site in NESTED_LIST:
+        if site[0] in op_link and not submission.id in already_done:
+  #          print "found link \n ========== "
+  #          print submission.id + "\n\n"
             already_done.append(submission.id)
     
-    for p in already_done: print "Completed: \n " + p
+  #  for p in already_done: print "Completed: \n " + p
 
 def processSelfPost(submission):
     op_text = submission.selftext.lower()
@@ -61,29 +63,28 @@ def linksFromText(post):
     links_found = []
     link_text = []
 
-    """# Catalog all locations of links to games
-    for site in SITE_LIST:
-        if site in post:
-            links_found.append(post.index(site))
-    links_found.sort()
-"""
-
-    # See where each links and use it generate the url of the DL link
-    #for i in links_found:
+    # iterate through text
     for i in range(0, len(post)-15):
-        token = post[i:i+15]
-        if token == "www.chess.com/l":
-            link_text.append("http://www.chess.com/livechess/download_pgn?id=" + post[i+32:i+40])
-            print "found chess.com/e"
-        elif token == "www.chess.com/e":
-            link_text.append("http://www.chess.com/echess/download_pgn?id=" + post[i+29:i+37])
-            print "found www.chess.com/e"
-        elif token == "www.chessgames.":
-            link_text.append("http://www.chessgames.com/perl/nph-chesspgn?text=1&gid=" + post[i+38:i+45])   
-            print "found chessgames"
-        elif token == "en.lichess.org/":
-            link_text.append("http://en.lichess.org/" + post[i+15:i+23] + "/pgn")
-            print "found lichess"
+        # compare 15- character segments 
+        token =  post[i:i+15]
+        for n in NESTED_LIST:
+            # generate vars from NESED_LIST and iterator
+            lURL=n[0]
+            pURL=n[1]
+            idStart=i+n[2]
+            idEnd=idStart+n[3]
+            gameid = post[idStart:idEnd]
+            pgnCreated = '' 
+
+            print n[2]
+            print len(n[1])
+            print
+
+            if token == lURL:
+                pgnCreated = pURL + gameid + n[4]
+                print "found link: %s" % pgnCreated
+                link_text.append(pgnCreated)                
+
     for a in link_text:
         print a
  
@@ -105,4 +106,5 @@ def postPgn(links):
 print 'preparing to go'
 go()
 
-
+# print " testing nesting"
+nest = [('www.chess.com/livechess/game', 'http://www.chess.com/livechess/download_pgn?id=', '9' , ''),('www.chess.com/echess/game', 'http://www.chess.com/echess/download_pgn?id=', '9', ''),('www.chessgames.com/perl/chessgame', 'http://www.chessgames.com/perl/nph-chesspgn?text=1&gid=', '8', ''),('en.lichess.org/', 'http://en.lichess.org/', '9', '/pgn' )]
