4b32128add7b63050b5ee5b355ed847e598cdd7c
rastalas <ianmcgeoch@gmail.com>
2014-01-11 17:49:22 -0500

    Added logging and persistent completed_posts

--- config.py
+++ config.py
@@ -3,14 +3,11 @@ config = {
     'username' : 'PGN-Bot',
     'password' : '' ,
     'user_agent' : 'PGN-Bot is a bot to reply to links in /r/chess with text-based versions of the games',
-# nested_list is used for comparing and generating links. 
-# for each site to process links from, provide 5 variables
-# 
-# [0] the beginning of a url, common to all games on that site
-# [1] the beginning of a url, common to all pgn files of games on that site
-# [2] the length of the unique id
-# [3] the common url following the unique id of all pgns, if any
-    'nested_list' : [('http://www.chess.com/livechess/game?id=', 'http://www.chess.com/livechess/download_pgn?id=', 9 , ''),('http://www.chess.com/echess/game?id=', 'http://www.chess.com/echess/download_pgn?id=', 9, ''),('http://www.chessgames.com/perl/chessgame?gid=', 'http://www.chessgames.com/perl/nph-chesspgn?text=1&gid=', 8, ''),('en.lichess.org/', 'http://en.lichess.org/', 8, '/pgn' )],
+
+# Three regular expressions are needed:
+# 1. One that matches a link to a game, without group names, for searching
+# 2. One that matches a link to a game, with group names, for subbing
+# 3 One that matches a .pgn download link, with flags, for subbing
     'regex_list' : [( 'http://www\.chess\.com/l*i*v*echess/game\?id=[0-9]{8,9}','http://www\.chess\.com/(?P<gmtype>l*)i*v*echess/game\?id=(?P<id>[0-9]{8,9})', 'http://www.chess.com/echess/download_pgn?\g<gmtype>id=\g<id>'),( 'http://www\.chessgames\.com/perl/chessgame\?gid=[0-9]{7}', 'http://www\.chessgames\.com/perl/chessgame\?gid=(?P<id>[0-9]{7})', 'http://www.chessgames.com/perl/nph-chesspgn?text=1&gid=\g<id>'),('http://[a-z]{2}\.lichess\.org/[a-z0-9]{8}','http://(?P<lang>[a-z]{2})\.lichess\.org/(?P<id>[a-z0-9]{8})','http://\g<lang>.lichess.org/\g<id>/pgn')]
 
 
--- pgn-link-bot.py
+++ pgn-link-bot.py
@@ -10,61 +10,79 @@ from config import config
 import praw
 import urllib2
 import re
-
+import datetime
 
 # define local variables
 USERNAME = config['username']
 PASSWORD = config['password']
 USER_AGENT = config['user_agent']
 REGEX_LIST = config['regex_list']
-NESTED_LIST = config['nested_list']
 
-reddit = praw.Reddit(user_agent = USER_AGENT)
-reddit.login(username = USERNAME, password = PASSWORD)
 already_done = []     
-
-print "logging in to reddit"
 
 
 def go():
+    log.write("Starting scan...\n")
+    log.write(str(datetime.datetime.now())+"\n")
     subreddit = reddit.get_subreddit('chessbottesting')  
-    for  submission in subreddit.get_new(limit=10):
-        
-        print "Checking " + submission.id
-        if not submission.is_self:
-            print "id'd as link post"
-            processLinkPost(submission)
+    for submission in subreddit.get_new(limit=10):
+        # print "Checking " + submission.id
+        log.write("Checking " + submission.id +"\n")
+        if submission.id in already_done:
+            log.write("%s is already done\n" % submission.id)
         else:
-            print "id'd as self post"
-            processSelfPost(submission)
+            if not submission.is_self:
+                # print "identified as link post"
+                log.write("identified as link post\n")
+                processLinkPost(submission)
+            else:
+                # print "identified as self post"
+                log.write("identified as self post\n")
+                processSelfPost(submission)
 
     for submission in subreddit.get_hot(limit=25):
         flat_comments = praw.helpers.flatten_tree(submission.comments)
         for comment in flat_comments:
-            print "found comment %s" % comment.id
+            # print "found comment %s" % comment.id
+            log.write("found comment %s \n" % comment.id)
+            if comment.id in already_done:
+                log.write("%s is already done" % comment.id)
+                break
             processComment(comment)
-    print "Completed"
-    for p in already_done: print p
+    # print "\nCompleted: "
+    log.write("\nCompleted: \n")
+    log.writelines(already_done)
+    with open("completed_posts", 'w') as f:
+        f.write('\n'.join(already_done))    
 
 def processLinkPost(submission):
     op_link = submission.url.lower()
-
-    if linkToPgn(op_link):
-        pgn=linkToPgn(op_link)   
-        print "found game at %s" %  submission.url
-        print "created pgn link: %s" % pgn[0]
+    pgn=linkToPgn(op_link)   
+    
+    if pgn:
+        # print "found game at %s" %  submission.url
+        # print "created pgn link: %s" % pgn[0]
+        log.write("found game at %s\n" %  submission.url)
+        log.write( "created pgn link: %s\n" % pgn[0])
         postPgn(submission, pgn)
     else:
-        print "no chess game at %s" % submission.url
+        # print "no chess game at %s" % submission.url
+        log.write("no chess game at %s\n" % submission.url)
+    already_done.append(submission.id)
 
 def processSelfPost(submission):
     op_text = submission.selftext.lower()
     link_list = []
-    link_list = linksFromText(op_text)
+    link_list = linkToPgn(op_text)
     for i in link_list:
-        print "links from selftext: " + i
+        # print "Game found in  selftext %s: " % submission.id + i 
+        log.write( "Game found in  selftext %s: " % submission.id + i +"\n") 
+    
     if link_list:
-        postPgn(submission, link_list) 
+        postPgn(submission, link_list)
+    else:
+        # print "No games in %s" % submission.id 
+        log.write("No games in %s\n" % submission.id)
     already_done.append(submission.id)
 
 def processComment(comment):
@@ -72,9 +90,16 @@ def processComment(comment):
     link_list = []
     link_list = linksFromText(comment_text)
     for i in link_list:
-        print "links from comment: " + i
+        # print "Game found in comment %s: " % comment.id + i
+        log.write( "Game found in comment %s: \n" % comment.id + i)
+    
     if link_list:
         postPgn(comment, link_list)
+    else:
+        # print "No games in %s" % comment.id
+        log.write( "No games in %s \n" % comment.id)
+
+    
     already_done.append(comment.id)
 
 def linksFromText(post):
@@ -94,38 +119,6 @@ def linkToPgn(post):
             linksCreated.append(re.sub(n[1], n[2], link))
     return linksCreated
 
-
-def oldlinkToPgn(post):
-    pgnsCreated = []
-    for n in NESTED_LIST:
-        linkURL=n[0]
-        pgnURL=n[1]
-        urlLength = len(linkURL)
-        # iterate through text
-        for i in range(0, len(post)-urlLength):
-            # generate vars from NESED_LIST and iterator
-            token =  post[i:i+urlLength]
-            idStart=i+urlLength
-            idEnd=idStart+n[2]
-            gameid = post[idStart:idEnd]
-            newPgn = '' 
-
-            if token == linkURL:
-                # print "found link %s" % linkURL + gameid
-                newPgn = pgnURL + gameid + n[3]
-                newPgn = fixErrors (newPgn)
-                pgnsCreated.append(newPgn)
-                # print "created pgn: %s" % newPgn
-    return pgnsCreated
-
-def fixErrors(url):
-    # fixes problematic chess.com urls. I could probably use
-    # a similar approach to avoid linkToPgn() entirely 
-    print "\n\nregex should change %s" % url
-    url = re.sub('livechess', 'echess', url) 
-    print "to %s" % url
-    return url
-
 def getPgn(target):
     response = urllib2.urlopen(target)
     html = response.read()
@@ -133,14 +126,36 @@ def getPgn(target):
 
 def postPgn(parent, links):
     pgn = [] 
-
+    
     for link in links:
         pgn.append(getPgn(link))
     singlePgn = '\n'.join(pgn)
 
     post = "[pgn]\n" + singlePgn + "\n[/pgn]"
-    print post       
-    print link
+    # print post       
+
+
+
+print "Opening logfile"
+log = open('logfile' , 'a')
+log.write("\n\n Starting up...\n\n")
+
+
+print "Opening completed_posts"
+log.write("Opening completed posts\n")
+#done = open('completed_posts' , 'r+')
+#already_done = done.readlines()
+#print done
+
+
+already_done = [line.strip() for line in open('completed_posts')]
+
+
+print "logging in to reddit"
+log.write("Logging in to reddit...\n")
+reddit = praw.Reddit(user_agent = USER_AGENT)
+reddit.login(username = USERNAME, password = PASSWORD)
+
  
 print 'preparing to go'
 go()
