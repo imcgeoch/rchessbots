1c84eccca623e8fcd7ad0afb77f8eb2849c27fcd
rastalas <ianmcgeoch@gmail.com>
2014-01-12 23:37:05 -0500

    Appears to be in working order. Haven't tested running for long periods or how it handles reddit crashing.

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
 rchessbots
 ==========
 
-Reddit bots to post formatted pgns for easier viewing
+Reddit bot to post formatted pgns for easier viewing
+
+
+As of January 12, 2014, this reddit bot reads through 
+selected subreddits and finds links to chess games in
+submissions and comments. When it finds a link to a 
+supported site, it grabs the pgn file from that game and
+posts it as a comment posted for the chess.com pgn viewer.
+
+The [pgn viewer](http://www.reddit.com/r/chess/comments/1s8j14/inline_pgn_viewer_faq/) is a plugin for [Firefox](https://addons.mozilla.org/en-US/firefox/addon/rchess-pgn-viewer/) and [Chrome](https://chrome.google.com/webstore/detail/hplecpnihkigeaiobbmfnfblepiadjdh) that allows you to view games inside reddit.
+It was created by somebody else and has no affiliation
+with this bot.
+
+As of now, this bot does not have other functionality
+that some discussed on r/chess, such as finding and
+fixing posts of pgns, or aggregating posts for a weekly digest.
+It could integrate such features if they were written.
+
+Logs are kept so that the operator can check on what the 
+program was doing in case of bugs etc.
+
+A configuration file, config.py, allows the operator to 
+choose the username, password, API agent, and subreddits
+to operate on. The text that included in each post and
+supported sites can also be changed. Every supported site 
+needs three regular expressions:
+1. One that matches a link to a game, without group names, for searching
+2. One that matches a link to a game, with group names, for subbing
+3. One that matches a .pgn download link, with group, for subbing
+
+All the sites supported use unique game ids for each game
+in standard link formats. This passes the id from a
+game link to a pgn link. 
--- pgn-link-bot.py
+++ pgn-link-bot.py
@@ -1,9 +1,32 @@
 
 """ 
-A reddit bot that will reply to links to chess games on a number 
-of off-reddit sites, take the pgns from those sites, and respond
-with a comment containing the pgn in [pgn] tags for the veiwer plugin.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
 
+    (1) Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer. 
+
+    (2) Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in
+    the documentation and/or other materials provided with the
+    distribution.  
+    
+    (3)The name of the author may not be used to
+    endorse or promote products derived from this software without
+    specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
 """
 
 from config import config
@@ -13,105 +36,90 @@ import re
 import datetime
 import time
 
-# define local variables
+# define variables
 USERNAME = config['username']
 PASSWORD = config['password']
 USER_AGENT = config['user_agent']
+SUBREDDIT_LIST = config['subreddit_list']
 REGEX_LIST = config['regex_list']
 POST_TEXT = config['post_text']
 
-already_done = []     
 
-
+# define methods
 def go():
     log.write("Starting scan...\n")
     log.write(str(datetime.datetime.now())+"\n")
-    subreddit = reddit.get_subreddit('chessbottesting')  
+    for s in SUBREDDIT_LIST:
+        subreddit = reddit.get_subreddit(s)  
+        doPosts(subreddit)   
+        doComments(subreddit)
+    
+    log.write("\nCompleted pass, sleeping for 5 minutes\n\n\n")
+    print "Completed pass, sleeping for 5 minutes"
+    time.sleep(300)
+
+def doPosts(subreddit):
     for submission in subreddit.get_new(limit=10):
-        # print "Checking " + submission.id
         log.write("Checking " + submission.id +"\n")
         commenters = []
         for x in submission.comments:
             commenters.append(x.author.name)
-            print x.author.name + str(x.author.name == 'PGN-Bot')
         if "PGN-Bot" in commenters:
             log.write("%s is already done\n" % submission.id)
         else:
             if not submission.is_self:
-                # print "identified as link post"
                 log.write("identified as link post\n")
                 processLinkPost(submission)
             else:
-                # print "identified as self post"
                 log.write("identified as self post\n")
                 processSelfPost(submission)
 
-    for submission in subreddit.get_hot(limit=25):
-        flat_comments = praw.helpers.flatten_tree(submission.comments)
-        for comment in flat_comments:
-            # print "found comment %s" % comment.id
-            log.write("found comment %s \n" % comment.id)
-            commenters = []
-            for x in comment.replies:
-                print x
-                commenters.append(x)
-            if "PGN-Bot" in commenters:
-             
-                log.write("%s is already done" % comment.id)
-                break
-            processComment(comment)
-    # print "\nCompleted: "
-    log.write("\nCompleted: \n")
-    log.writelines(already_done)
-    with open("completed_posts", 'w') as f:
-        f.write('\n'.join(already_done))    
+def doComments(subreddit):
+    flat_comments = subreddit.get_comments()
+    for comment in flat_comments:
+        log.write("found comment %s \n" % comment.id)
+        commenters = []
+        for x in comment.replies:
+            commenters.append(x.author.name)
+        if "PGN-Bot" in commenters:
+            
+            log.write("%s is already done" % comment.id)
+            break
+        processComment(comment)
 
 def processLinkPost(submission):
     op_link = submission.url.lower()
     pgn=linkToPgn(op_link)   
     
     if pgn:
-        # print "found game at %s" %  submission.url
-        # print "created pgn link: %s" % pgn[0]
         log.write("found game at %s\n" %  submission.url)
-        log.write( "created pgn link: %s\n" % pgn[0])
-        postPgn(submission, pgn, submission.add_comment)
+    #    log.write( "created pgn link: %s\n" % pgn[0])
+        postPgn(pgn, submission.add_comment)
     else:
-        # print "no chess game at %s" % submission.url
         log.write("no chess game at %s\n" % submission.url)
-    already_done.append(submission.id)
 
 def processSelfPost(submission):
     op_text = submission.selftext.lower()
     link_list = []
     link_list = linkToPgn(op_text)
     for i in link_list:
-        # print "Game found in  selftext %s: " % submission.id + i 
-        log.write( "Game found in  selftext %s: " % submission.id + i +"\n") 
-    
+        log.write( "Game found in selftext %s: " % submission.id + i +"\n") 
     if link_list:
-        postPgn(submission, link_list, submission.add_comment)
+        postPgn(link_list, submission.add_comment)
     else:
-        # print "No games in %s" % submission.id 
         log.write("No games in %s\n" % submission.id)
-    already_done.append(submission.id)
 
 def processComment(comment):
     comment_text = comment.body.lower()
     link_list = []
     link_list = linksFromText(comment_text)
     for i in link_list:
-        # print "Game found in comment %s: " % comment.id + i
         log.write( "Game found in comment %s: \n" % comment.id + i)
     
     if link_list:
-        postPgn(comment, link_list, comment.reply)
+        postPgn(link_list, comment.reply)
     else:
-        # print "No games in %s" % comment.id
         log.write( "No games in %s \n" % comment.id)
-
-    
-    already_done.append(comment.id)
 
 def linksFromText(post):
     link_text = []
@@ -122,12 +130,11 @@ def linksFromText(post):
 def linkToPgn(post):
     linksCreated = []
     for n in REGEX_LIST:
-        # print n[0] + " 1"
         linksFound = re.findall(n[0], post)
-        # print linksFound
         for link in linksFound:
-            # print link 
-            linksCreated.append(re.sub(n[1], n[2], link))
+            newLink = re.sub(n[1], n[2], link)
+            log.write("Created link to %s\n" % newLink)
+            linksCreated.append(newLink)
     return linksCreated
 
 def getPgn(target):
@@ -135,35 +142,33 @@ def getPgn(target):
     html = response.read()
     return html
 
-def postPgn(parent, links, methodtorun):
+def postPgn(links, postmethod):
     pgn = [] 
-    
     for link in links:
         pgn.append(getPgn(link))
     singlePgn = '\n'.join(pgn)
-
     post = "[pgn]\n" + singlePgn + "\n[/pgn]"
     while True:
         try:
-            methodtorun(post + POST_TEXT)       
+            newPost = postmethod(post + POST_TEXT)
+            log.write("Successfully posted: %s\n" % newPost.id)       
             break
         except praw.errors.RateLimitExceeded as error:
-            print "Waiting %d seconds to be allowed to post" % (error.sleep_time/2)
-            time.sleep(error.sleep_time/2)
+            log.write( "Waiting %d seconds to be allowed to post\n" % min(error.sleep_time, 270))
+            time.sleep(min(error.sleep_time, 270))
+
+# Begin Script
 
 print "Opening logfile"
 log = open('logfile' , 'a')
 log.write("\n\n Starting up...\n\n")
 
-
-
-
 print "logging in to reddit"
 log.write("Logging in to reddit...\n")
 reddit = praw.Reddit(user_agent = USER_AGENT)
 reddit.login(username = USERNAME, password = PASSWORD)
-
  
 print 'preparing to go'
-go()
+while True:
+    go()
 
